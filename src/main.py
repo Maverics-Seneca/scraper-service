efb87699ff2f2ea2cd32cf5922224301dc2a34ad
Renju <84337556+R3NJU@users.noreply.github.com>
2025-03-12 17:41:43 -0400

    Added Side Effects

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiohttp
-from lxml import html
+from lxml import html, etree
 import json
 import csv
 # import pandas as pd
@@ -18,8 +18,12 @@ def parse_response(resp):
     tree = html.fromstring(resp.content)
     title = tree.xpath('//div[@class="cmp-title"]/h1/text()')[0]
     description = ''.join(tree.xpath('//div[@id="drug-description"]/p/text()'))
+    side_effects_tree = tree.xpath('//div[@id="drug-side-effects"]')[0]
+    side_effects_raw = ''.join([etree.tostring(e, encoding='unicode', method='html') for e in side_effects_tree[1:]])
+    side_effects = side_effects_tree.xpath('.//li//text()')
     
-    return {"Name":title, "Description":description}
+    
+    return {"name":title, "description":description, "side_effects":side_effects, "side_effects_raw": side_effects_raw}
 
 def main():
     urls = fetch_sitemap()
