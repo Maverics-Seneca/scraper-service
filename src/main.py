4e2326510f69b3853e13e6f40e21f8bcb7d7c7e8
Renju <84337556+R3NJU@users.noreply.github.com>
2025-03-11 20:27:34 -0400

    Fetching Sitemap XML

--- src/main.py
+++ src/main.py
@@ -3,6 +3,20 @@ import aiohttp
 from lxml import html
 import json
 import csv
+# import pandas as pd
+import xml.etree.ElementTree as ET
+import requests
+
+def fetch_sitemap():
+    sitemap_response = requests.get('https://www.mayoclinic.org/patient_consumer_drug.xml')
+    root = ET.fromstring(sitemap_response.content)
+    namespaces = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+    urls = [elem.text for elem in root.findall('.//sitemap:loc', namespaces)]
+    return urls
+
+print(len(fetch_sitemap()))
+
+start_url = ''
 
 def main():
     pass
